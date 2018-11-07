c2891d52fee7a2897a47f530f1ac6fcb64a886a4
kalozfauzan <kaloz.fauzan2@gmail.com>
2018-11-07 20:51:58 +0700

    initial commit

---
+++ src/page/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class App extends React.Component<any>{
+
+
+    render(){
+        return(
+            <div className="title">
+                <h1 className="title__hello">Hello I'm React With Typescript And Parcel JS</h1>
+            </div>
+        )
+    }
+}
